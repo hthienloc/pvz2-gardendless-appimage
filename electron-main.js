e114c5424b10618c709543c6360d6c3b7b971d73
loccun <huynhloc.contact@gmail.com>
2026-04-09 17:11:51 +0700

    feat: migrate to Electron and add .gitignore

---
+++ electron-main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    icon: path.join(__dirname, 'assets/icons/icon.png'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Chạy game từ thư mục dist
+  win.loadFile(path.join(__dirname, 'dist/index.html'));
+
+  // Không hiển thị menu bar mặc định
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
