1e8fd42795a80f1dd0cbe6164ae8a65b617c708a
loccun <huynhloc.contact@gmail.com>
2026-04-09 16:27:41 +0700

    chore: update application icons, restrict build targets, and configure webview environment variables and CSP

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  #[cfg(target_os = "linux")]
+  {
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+  }
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
